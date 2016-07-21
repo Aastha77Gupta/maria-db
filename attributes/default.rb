43b37e8ab861fefd07533bb20bd1a19dd8f0b6d9
Aastha77Gupta <aastha3@gmail.com>
2016-07-21 09:26:53 -0500

    Update default.rb

--- attributes/default.rb
+++ attributes/default.rb
@@ -19,3 +19,6 @@ default['mariadb']['connect_timeout']               = '5'
 
 #innoDB
 default['mariadb']['innodb']['buffer_pool_size']    = '1G'
+
+# MariaDB / Galera Cluster Settings
+wsrep_provider_options          = "socket.checksum=1"
